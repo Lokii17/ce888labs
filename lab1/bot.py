67fba31f0ae17d8b8230e7ce537ba75f1dbb72a5
Lokii17 <fn17226@essex.ac.uk>
2018-01-23 12:15:25 +0000

    lab1

---
+++ lab1/bot.py
@@ -0,0 +1,12 @@
+from sopel import module
+from emo.wdemotions import EmotionDetector
+
+emo = EmotionDetector()
+
+@module.rule('')
+def hi(bot, trigger):
+    print(trigger, trigger.nick)
+    #bot.say('Hi, ' + trigger.nick)
+    ave = emo.detect_emotion_in_raw_np(str(trigger))
+    print(ave)
+
