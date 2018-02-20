2beb2c82733e5f46b2d2bf9a311cedc2795b0e6a
Lokii17 <fn17226@essex.ac.uk>
2018-02-20 14:55:44 +0000

    Assignments

--- Assignments/Assignment1/Task1.py
+++ Assignments/Assignment1/Task1.py
@@ -6,8 +6,7 @@ import pandas as pd
 def create_csv(data, name):
     #scale data and reshape to vektors (arrays)
     (x_train, y_train), (x_test, y_test) = data
-    x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
+    print(x_train.shape)
     x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
     x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
@@ -17,33 +16,33 @@ def create_csv(data, name):
     tasks_n = 3    
     mask = np.arange(len(x_train[0]))
     shuffles = []
-    #creats n tasks with different shuffeling, the shuffeling is saved in shuffles
+
+    #save all data in for_file to convert this array to a file, the y-value is in the last column.
+    for_file = np.zeros(shape=(len(y_train)+len(y_test), len(mask)+1))
+    #the loop creats n tasks with different shuffeling, the shuffeling is saved in shuffles
     for i in range(tasks_n):
         #print this values after shuffeling
         np.random.shuffle(mask)
         shuffles.append(list(mask))
         print(x_train[0][shuffles[i][320:323]])
 
-        #save all data in for_csv to convert this array to a csv file, the y-value is in the last column
-        for_csv = np.zeros(shape=(len(y_train)+len(y_test), len(mask)+1))
         for j in range(len(y_train)):
-            for_csv[j][0:len(mask)] = x_train[j][shuffles[i]]
-            for_csv[j][len(mask)] = y_train[j]            
+            for_file[j][0:len(mask)] = x_train[j][shuffles[i]]
+            for_file[j][len(mask)] = y_train[j]            
             
         for j in range(len(y_test)):
-            for_csv[len(y_train)+j][0:len(mask)] = x_test[j][shuffles[i]]
-            for_csv[len(y_train)+j][len(mask)] = y_test[j]
+            for_file[len(y_train)+j][0:len(mask)] = x_test[j][shuffles[i]]
+            for_file[len(y_train)+j][len(mask)] = y_test[j]
         
-        #print this values in for_csv (equal to after shuffleing)
-        print(for_csv[0][320:323])
+        #print this values in for_file (equal to after shuffleing)
+        print(for_file[0][320:323])
 
-        print("shape of for_csv:")
-        print(for_csv.shape)
+        print("shape of for_file:")
+        print(for_file.shape)
 
         #now save everything in an csv file
-        print("start to save")        
-        df = pd.DataFrame(for_csv)
-        df.to_csv(name + str(i+1) + ".csv")
+        print("start to save")
+        np.save(name + str(i+1), for_file)  
         print("file saved")
 
 
